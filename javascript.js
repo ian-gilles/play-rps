f6a2062e70e3362196c9a1b8cbe926a9be266e8d
Ian <idgilles@gmail.com>
2022-10-03 21:36:44 -0400

    Add function getComputerChoice()

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
-let test = console.log("Hello World!");
+function getComputerChoice() {
+    let randomInteger = Math.floor(Math.random() * 3);
+
+    if (randomInteger === 0) {
+        return "rock"
+    } 
+    else if (randomInteger === 1) {
+        return "paper"
+    } 
+    else {
+        return "scissors"
+    };
+}
