a6b4309144099b380849ecb47fbdab27912e607d
Ian <idgilles@gmail.com>
2022-10-04 20:12:04 -0400

    Add fuction playRound() to play a round of RPS

--- javascript.js
+++ javascript.js
@@ -11,3 +11,30 @@ function getComputerChoice() {
         return "scissors"
     };
 }
+
+function getPlayerSelection() {
+    let playerInput = prompt("Rock, paper, or scissors?", "Rock")
+    return playerInput.toLowerCase()
+}
+
+function playRound() {
+    let playerSelection = getPlayerSelection();
+    let computerSelection = getComputerChoice();
+
+    if (playerSelection === computerSelection) {
+        return `You tie! Computer chose ${computerSelection}.`
+    }
+    else if ((playerSelection === 'rock' && computerSelection === 'scissors')
+        || (playerSelection === 'paper' && computerSelection === 'rock')
+        || (playerSelection === 'scissors' && computerSelection === 'paper')) {
+            return `You win! Computer chose ${computerSelection}.`
+        }
+    else if ((computerSelection === 'rock' && playerSelection === 'scissors')
+        || (computerSelection === 'paper' && playerSelection === 'rock')
+        || (computerSelection === 'scissors' && playerSelection === 'paper')) {
+            return `You lose! Computer chose ${computerSelection}.`
+        }
+    else {
+        return `Sorry "${playerSelection}" is not a valid choice. Please try again.`
+    }
+}
